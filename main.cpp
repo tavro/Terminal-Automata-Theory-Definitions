79bd36ea254ba44eed004e4a350fa8cbcf0846b5
Isak Horvath <isakhorvath@gmail.com>
2022-09-14 16:35:06 +0200

    NEW: implement fileformat for reading pages

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "colormod.h"
+#include "string.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 void print_title(string title) {
@@ -8,49 +10,41 @@ void print_title(string title) {
 	cout << bg_red << " " << title << " " << bg_def << "\n" << endl;
 }
 
-void print_definition(int num) {
+void print_definition(string num) {
 	Color::Modifier fg_red(Color::FG_RED);
 	cout << fg_red << " Definition " << num << endl;
 }
 
 void print_text(string text) {
 	Color::Modifier fg_def(Color::FG_DEFAULT);
-	cout << fg_def << text << "\n" << endl;
+	cout << fg_def << " " << text << "\n" << endl;
 }
 
 void print_text_no_endl(string text) {
 	Color::Modifier fg_def(Color::FG_DEFAULT);
-        cout << fg_def << text << "\n";
+        cout << fg_def << " " << text << "\n";
 }
 
 int main() {
-	print_title("Automata Theory");
-	print_definition(1);
-	print_text(" An alphabet is a finite set of symbols (denoted by Σ)");
-	print_definition(2);
-	print_text(" A string over an alphabet Σ is a finite sequence of symbols over Σ i.e. each symbol is included in Σ");
-	print_definition(3);
-	print_text(" The length of a string x, written |x| is the number of symbols in x");
-	print_definition(4);
-	print_text(" The empty string ε is the string of length 0");
-	print_definition(5);
-	print_text(" The concatenation of x and y is written xy");
-	print_definition(6);
-	print_text_no_endl(" Let x be a string");
-	print_text_no_endl(" • x^0 = ε");
-	print_text(" • x^k = x...x, for k>=1");
-	print_definition(7);
-	print_text(" Let Σ be an alphabet. We let Σ* = {x | x is a string over Σ} be the set of strings over Σ");
-	print_definition(8);
-	print_text(" Let Σ be an alphabet. A set X⊆ Σ* is called a formal language, or simply a language");
-	print_definition(9);
-	print_text_no_endl(" Let A and B be languages over an alphabet Σ. We define the following set operations:");
-	print_text_no_endl(" 1. A ∩ B = {x | x ∈  A, x ∈  B} (intersection)");
-	print_text_no_endl(" 2. A ∪ B = {x | x ∈  A or x ∈  B} (union)");
-	//print_text_no_endl(" 3. A = {} (complement)");
-	//print_text_no_endl(" 4. P(A) = 2^A = {} (powerset)");
-	//print_text_no_endl(" 5. AB = {} (concatenation)");
-	//print_text_no_endl(" 6. A^0 = {} and A^k = A...A, k>=1 (kth power)");
-	//print_text_no_endl(" 7. A* = A^0 A^1 A^2 ... (star operation)");
-	//print_text(" 8. A+ = AA* = A^n (union of non-zero power)");
+	ifstream input("definitions1.tl");
+	for(string line; getline(input, line);) {
+		char symbol = line[0];
+		string content = line.substr(1, line.length());
+		switch(symbol) {
+			case '#':
+				print_title(content);
+				break;
+			case '$':
+				print_definition(content);
+				break;
+			case '&':
+				print_text_no_endl(content);
+				break;
+			case '@':
+				print_text(content);
+				break;
+			default:
+				print_text(line);
+		}
+	}
 }
